35c37cef8fbb481e532a3b61c2808b8cc90676e3
malli <mallikarjuna2314@gmail.com>
2025-05-23 10:59:04 +0530

    special vars

--- 04-special-variables.sh
+++ 04-special-variables.sh
@@ -3,3 +3,10 @@
 
 echo "All variables passed : $@"
 echo "number variables : $#"
+echo "Script number : $0"
+echo "Current directory : $PWD "
+echo "User running the script : $USER"
+echo "home directory of user : $HOME"
+echo "PID of the script : $$"
+sleep 100 &
+echo "PID of the script running in the bg : $!"
