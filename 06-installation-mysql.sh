241af4b32019ffe7a131add1a138c0bf95b450b2
malli <mallikarjuna2314@gmail.com>
2025-05-23 11:46:49 +0530

    special vars

---
+++ 06-installation-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+uid=$(id -u)
+
+if [ uid -ne 0 ] 
+then 
+    echo "you are not running with root user"
+    exit 1
+else 
+    echo "you are running with root user"
+fi
+
+dnf install mysql -y
