69c6b35a16f9b413e7b9430f0880c82c5bac909e
malli <mallikarjuna2314@gmail.com>
2025-05-23 21:05:58 +0530

    colours

--- 10-logs.sh
+++ 10-logs.sh
@@ -11,49 +11,49 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at : $(date)" &>>$LOG_FILE
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 if [ $uid -ne 0 ] 
 then 
-    echo -e "$R you are not running with root user $N" &>>$LOG_FILE
+    echo -e "$R you are not running with root user $N" | tee -a $LOG_FILE
     exit 1
 else 
-    echo "you are running with root user" &>>$LOG_FILE
+    echo "you are running with root user" | tee -a $LOG_FILE
 fi
 
 validate(){
     if [ $1 -eq 0 ]
     then 
-        echo -e "Installing $2 is $G success $N " &>>$LOG_FILE
+        echo -e "Installing $2 is $G success $N " | tee -a $LOG_FILE
     else 
-        echo -e "Installing $2 is $R not succcess $N" &>>$LOG_FILE
+        echo -e "Installing $2 is $R not succcess $N" | tee -a $LOG_FILE
     fi
 }
 dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo "mysql is not installed...going to install" &>>$LOG_FILE
+    echo "mysql is not installed...going to install" | tee -a $LOG_FILE
     dnf install mysql -y &>>$LOG_FILE
     validate $? "mysql"
 else 
-    echo "already installed" &>>$LOG_FILE
+    echo "already installed" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo "nginx is not installed...going to install"&>>$LOG_FILE
+    echo "nginx is not installed...going to install" | tee -a $LOG_FILE
     dnf install nginx -y &>>$LOG_FILE
     validate $? "nginx"
 else 
-    echo "already installed"&>>$LOG_FILE
+    echo "already installed" | tee -a $LOG_FILE
 fi
 
 dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo "python3 is not installed...going to install" &>>$LOG_FILE
+    echo "python3 is not installed...going to install" | tee -a $LOG_FILE
     dnf install python3 -y &>>$LOG_FILE
     validate $? "python3"
 else 
-    echo "already installed" &>>$LOG_FILE
+    echo "already installed" | tee -a $LOG_FILE
 fi
