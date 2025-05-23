223007bb537cf49dc3b18b3adfa4570ef34acc53
malli <mallikarjuna2314@gmail.com>
2025-05-23 19:33:37 +0530

    colours

---
+++ 09-colours.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+uid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $uid -ne 0 ] 
+then 
+    echo -e "$R you are not running with root user"
+    exit 1
+else 
+    echo "you are running with root user"
+fi
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "Installing $2 is success "
+    else 
+        echo "Installing $2 is not succcess"
+        exit 1
+    fi
+}
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed...going to install"
+    dnf install mysql -y
+    validate $? "mysql"
+else 
+    echo "already installed"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed...going to install"
+    dnf install nginx -y
+    validate $? "nginx"
+else 
+    echo "already installed"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed...going to install"
+    dnf install python3 -y
+    validate $? "python3"
+else 
+    echo "already installed"
+fi
