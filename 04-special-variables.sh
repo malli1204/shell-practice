76cddcf7f3a204c579819ea8f10db48a5142f3e3
malli <mallikarjuna2314@gmail.com>
2025-08-11 22:11:48 +0530

    hello world

--- 04-special-variables.sh
+++ 04-special-variables.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-echo "All variables passed : $@"
-echo "number variables : $#"
-echo "Script number : $0"
-echo "Current directory : $PWD "
-echo "User running the script : $USER"
-echo "home directory of user : $HOME"
-echo "PID of the script : $$"
-sleep 100 &
-echo "PID of the script running in the bg : $!"
+echo "all variables passed to script : $@"
+echo "script name : $0"
+echo "present working directory : $PWD"
+echo "Home directory of the user : $HOME"
+echo "Which user is running the script : $USER"
+echo "PID of current script : $$"
+sleep 10&
+echo "PID of the last command running in bg : $!"
