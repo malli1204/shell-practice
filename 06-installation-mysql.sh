3d0c318bae642f02c9983d787875297a81e95386
malli <mallikarjuna2314@gmail.com>
2025-08-11 22:34:53 +0530

    hello world

--- 06-installation-mysql.sh
+++ 06-installation-mysql.sh
@@ -1,36 +1,96 @@
 #!/bin/bash
 
-uid=$(id -u)
+ID=$(id -u)
 
-if [ $uid -ne 0 ] 
+if [ $ID -ne 0 ]
 then 
-    echo "you are not running with root user"
+    echo "you are not running in root access"
     exit 1
-else 
-    echo "you are running with root user"
+else
+    echo "you are running in root access"
 fi
-
-dnf list installed mysql
+dnf installed list mysql
 if [ $? -ne 0 ]
 then 
-    echo "mysql is not installed...going to install"
+    echo "mysql is not installed.... going to install"
     dnf install mysql -y
     if [ $? -eq 0 ]
     then 
-        echo "Installing is success "
+        echo "mysql is installed properly"
     else 
-        echo "Installing is not succcess"
+        echo "mysql not installed..."
         exit 1
-    fi
-else 
-    echo "already installed"
+else
+    echo "mysql is installed nothing to do"
 
-# dnf install mysql -y
 
-# if [ $? -eq 0 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# uid=$(id -u)
+
+# if [ $uid -ne 0 ] 
 # then 
-#     echo "Installing is success "
-# else 
-#     echo "Installing is not succcess"
+#     echo "you are not running with root user"
 #     exit 1
+# else 
+#     echo "you are running with root user"
 # fi
+
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not installed...going to install"
+#     dnf install mysql -y
+#     if [ $? -eq 0 ]
+#     then 
+#         echo "Installing is success "
+#     else 
+#         echo "Installing is not succcess"
+#         exit 1
+#     fi
+# else 
+#     echo "already installed"
+
+# # dnf install mysql -y
+
+# # if [ $? -eq 0 ]
+# # then 
+# #     echo "Installing is success "
+# # else 
+# #     echo "Installing is not succcess"
+# #     exit 1
+# # fi
