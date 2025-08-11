89ba32405eac3bd6f736ec8fba7f87d45f981a65
malli <mallikarjuna2314@gmail.com>
2025-08-11 19:22:48 +0530

    hello world

--- 02-variables.sh
+++ 02-variables.sh
@@ -6,8 +6,8 @@ echo "$time"
 
 
 
- #person1=$1
- #person2=$2
+ person1=$1
+ person2=$2
 
- #echo "$person1 : hi i am $person1"
- #echo "$person2 : hi i am $person2"
+ echo "$person1 : hi i am $person1"
+ echo "$person2 : hi i am $person2"
