14a8cd480c7a9c6307b8f6b6eef2b3cf6d9f5131
malli <mallikarjuna2314@gmail.com>
2025-08-11 19:18:43 +0530

    hello world

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
- person1=$1
- person2=$2
+time=$(date)
 
- echo "$person1 : hi i am $person1"
- echo "$person2 : hi i am $person2"
+
+
+
+ #person1=$1
+ #person2=$2
+
+ #echo "$person1 : hi i am $person1"
+ #echo "$person2 : hi i am $person2"
