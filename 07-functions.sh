ca7a80c061c45b87dd65bd87cbb2928f947c4c05
malli <mallikarjuna2314@gmail.com>
2025-08-11 22:43:20 +0530

    hello world

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,49 +1,118 @@
 #!/bin/bash
 
-uid=$(id -u)
+ID=$(id -u)
 
-if [ $uid -ne 0 ] 
+if [ $ID -ne 0 ]
 then 
-    echo "you are not running with root user"
+    echo "you are not running in root access"
     exit 1
-else 
-    echo "you are running with root user"
+else
+    echo "you are running in root access"
 fi
-validate(){
+
+Validate(){
     if [ $1 -eq 0 ]
     then 
-        echo "Installing $2 is success "
-    else 
-        echo "Installing $2 is not succcess"
+        echo "$2 installed succesfully"
+    else
+        echo "$2 not installed"
         exit 1
     fi
 }
-dnf list installed mysql
+dnf installed list $2
 if [ $? -ne 0 ]
 then 
-    echo "mysql is not installed...going to install"
-    dnf install mysql -y
-    validate $? "mysql"
-else 
-    echo "already installed"
+    echo "$2 is not installed.... going to install"
+    dnf install $2 -y
+    Validate $? "mysql"
+    fi
+else
+    echo "mysql is installed nothing 
+fito do"
 fi
 
-dnf list installed nginx
-if [ $? -ne 0 ]
-then 
-    echo "nginx is not installed...going to install"
-    dnf install nginx -y
-    validate $? "nginx"
-else 
-    echo "already installed"
-fi
 
-dnf list installed python3
-if [ $? -ne 0 ]
-then 
-    echo "python3 is not installed...going to install"
-    dnf install python3 -y
-    validate $? "python3"
-else 
-    echo "already installed"
-fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# uid=$(id -u)
+
+# if [ $uid -ne 0 ] 
+# then 
+#     echo "you are not running with root user"
+#     exit 1
+# else 
+#     echo "you are running with root user"
+# fi
+# validate(){
+#     if [ $1 -eq 0 ]
+#     then 
+#         echo "Installing $2 is success "
+#     else 
+#         echo "Installing $2 is not succcess"
+#         exit 1
+#     fi
+# }
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not installed...going to install"
+#     dnf install mysql -y
+#     validate $? "mysql"
+# else 
+#     echo "already installed"
+# fi
+
+# dnf list installed nginx
+# if [ $? -ne 0 ]
+# then 
+#     echo "nginx is not installed...going to install"
+#     dnf install nginx -y
+#     validate $? "nginx"
+# else 
+#     echo "already installed"
+# fi
+
+# dnf list installed python3
+# if [ $? -ne 0 ]
+# then 
+#     echo "python3 is not installed...going to install"
+#     dnf install python3 -y
+#     validate $? "python3"
+# else 
+#     echo "already installed"
+# fi
