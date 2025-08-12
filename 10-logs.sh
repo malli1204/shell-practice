62278f86328f182c0ca373f6c9ce8c3ea963a194
malli <mallikarjuna2314@gmail.com>
2025-08-12 18:57:51 +0530

    colours

--- 10-logs.sh
+++ 10-logs.sh
@@ -6,11 +6,18 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript.logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+
+
+LOGS_FOLDER="/var/log/shellscript.log"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
+# LOGS_FOLDER="/var/log/shellscript.log"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+# mkdir -p $LOGS_FOLDER
 echo "script started executing at : $(date)" | tee -a $LOG_FILE
 if [ $uid -ne 0 ] 
 then 
