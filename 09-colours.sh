c2ee00b93a60ddb7585704153a9dc1f88069417e
malli <mallikarjuna2314@gmail.com>
2025-08-12 18:51:07 +0530

    colours

--- 09-colours.sh
+++ 09-colours.sh
@@ -12,43 +12,43 @@ then
     echo -e "$R you are not running with root user $N"
     exit 1
 else 
-    echo "$G you are running with root user $N"
+    echo -e "$G you are running with root user $N"
 fi
 validate(){
     if [ $1 -eq 0 ]
     then 
-        echo "$G Installing $2 is success $N"
+        echo -e "$G Installing $2 is success $N"
     else 
-        echo "$G Installing $2 is not succcess $N"
+        echo -e "$G Installing $2 is not succcess $N"
         exit 1
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-    echo "mysql is not installed...$Y going to install $N"
+    echo -e "mysql is not installed...$Y going to install $N"
     dnf install mysql -y
     validate $? "mysql"
 else 
-    echo "$G already installed $N"
+    echo -e "$G already installed $N"
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]
 then 
-    echo "nginx is not installed...$Y going to install $N"
+    echo -e "nginx is not installed...$Y going to install $N"
     dnf install nginx -y
     validate $? "nginx"
 else 
-    echo "$G already installed $N"
+    echo -e "$G already installed $N"
 fi
 
 dnf list installed python3
 if [ $? -ne 0 ]
 then 
-    echo "python3 is not installed...$Y going to install $N"
+    echo -e "python3 is not installed...$Y going to install $N"
     dnf install python3 -y
     validate $? "python3"
 else 
-    echo "$G already installed $N"
+    echo -e "$G already installed $N"
 fi
