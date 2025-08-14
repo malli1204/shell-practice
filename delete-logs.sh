cb0e4c41dbbe1d3733e039279bb629ab8543ace0
malli <mallikarjuna2314@gmail.com>
2025-08-14 22:46:36 +0530

    roboshop

--- delete-logs.sh
+++ delete-logs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-files=$(find . -name "*.log" -mtime +14)
+source_file=/home/ec2-user/app-logs
+
+files=$(find $source_file -name "*.log" -mtime +14)
 
 while IFS= read -r filepath
 do
